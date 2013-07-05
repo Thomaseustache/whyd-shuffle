d3b56e8302f5dc6e9951f4e6fe4031a38045715c
Thomaseustache <thomas-eustache@cifacom.com>
2013-07-05 15:02:24 +0200

    reorder

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
-$('.buttons').append('<button id="shuffle" type="button" value="1">Shuffle</button>');
+var orig = $(".posts").children(); //caching original
 
-$('#shuffle').bind('click',shuffle);
-
-function shuffle(){
-  var $button = $(".btnLoadMore:visible");
-  if ($button.length){
-    console.log('loadmore');
-    console.log(window.nextPageUrl);
-    
-    $button.find("div").addClass("loading");
-    var $frame = $button.parent();
-    $frame.ready(function() {
-      $.get(window.nextPageUrl+'&limit=9999', function(data) {
-        $button.remove();
-        $frame.append(data).ready(function() {
-          window.playem.updateTracks();
-          $(this).ajaxify();
-        });
+var $button = $(".btnLoadMore:visible");
+if ($button.length){
+  console.log('loadmore');
+  console.log(window.nextPageUrl);
+  
+  $button.find("div").addClass("loading");
+  var $frame = $button.parent();
+  $frame.ready(function() {
+    $.get(window.nextPageUrl+'&limit=9999', function(data) {
+      $button.remove();
+      $frame.append(data).ready(function() {
+        reorder();
       });
     });
-    
-    // window.playem.updateTracks();
-    // $(this).ajaxify();
-  }
-  else{
-    console.log('full');
-  }
-  console.log('on');
+  });
+}
+else{
+  reorder();
+}
+window.playem.updateTracks();
+$frame.ajaxify();
+console.log('on');
+
+function reorder() {
+    var grp = $(".posts").children();
+    var cnt = grp.length;
+
+    var temp, x;
+    for (var i = 0; i < cnt; i++) {
+        temp = grp[i];
+        x = Math.floor(Math.random() * cnt);
+        grp[i] = grp[x];
+        grp[x] = temp;
+    }
+    $(grp).remove();
+    $(".posts").append($(grp));
+    console.log('REORDER');
+}
+
+function orderPosts() {
+    // set original order
+    $(".posts").html(orig);
 }
