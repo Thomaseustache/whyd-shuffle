d56f4faef8870dd7e0acb736e62f3511c0af6631
Thomaseustache <thomas-eustache@cifacom.com>
2013-07-05 13:58:49 +0200

    delete if

--- script.js
+++ script.js
@@ -3,15 +3,7 @@ $('.buttons').append('<button id="shuffle" type="button" value="1">Shuffle</butt
 $('#shuffle').bind('click',shuffle);
 
 function shuffle(){
-  console.log('Shuffle Function');
-  if($('#shuffle').hasClass('on')){
     randomNext = Math.floor(Math.random() * trackList.length) + 1;
     trackList[(currentTrack.index + randomNext) % trackList.length];
-    console.log('OFF');
-    $('#shuffle').removeClass('on');
-  }
-  else {
-    console.log('ON');
-    $('#shuffle').addClass('on');
-  }
+    console.log('on');
 }
