a83a1b6401c691f9182300108921ce135947b7fa
Thomaseustache <thomas-eustache@cifacom.com>
2013-07-05 14:52:23 +0200

    add loadmore

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ $('.buttons').append('<button id="shuffle" type="button" value="1">Shuffle</butt
 $('#shuffle').bind('click',shuffle);
 
 function shuffle(){
-  var $btnLoadMore = $(".btnLoadMore:visible");
-  if ($btnLoadMore.length){
+  var $button = $(".btnLoadMore:visible");
+  if ($button.length){
     console.log('loadmore');
     console.log(window.nextPageUrl);
+    
+    $button.find("div").addClass("loading");
+    var $frame = $button.parent();
+    $frame.ready(function() {
+      $.get(window.nextPageUrl+'&limit=9999', function(data) {
+        $button.remove();
+        $frame.append(data).ready(function() {
+          window.playem.updateTracks();
+          $(this).ajaxify();
+        });
+      });
+    });
+    
     // window.playem.updateTracks();
     // $(this).ajaxify();
   }
